494322df753189a100c147a6e644798e2af05357
puja-03 <pujakumari11th2003@gmail.com>
2026-04-29 12:51:59 +0530

    feat: implement edit and update functionality for products in ProductController

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,8 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
     
     Route::get('products/create', [ProductController::class, 'create'])->name('products.create');
+    Route::get('products/{product}/edit', [ProductController::class, 'edit'])->name('products.edit');
+    Route::patch('products/{product}', [ProductController::class, 'update'])->name('products.update');
     Route::post('products', [ProductController::class, 'store'])->name('products.store');
     Route::get('admin/users', [UserController::class, 'index'])->name('admin.users')->middleware('admin');
     Route::patch('admin/users/{user}/role', [UserController::class, 'updateRole'])->name('admin.users.role')->middleware('admin');
