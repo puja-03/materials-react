0f461b8e0790f45e1c53a85bd55d148bd1e4b78f
puja-03 <pujakumari11th2003@gmail.com>
2026-04-29 13:19:26 +0530

    feat: add orderItems relationship to Product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -14,4 +14,9 @@ class Product extends Model
     {
         return $this->belongsTo(Category::class);
     }
+
+    public function orderItems(): \Illuminate\Database\Eloquent\Relations\HasMany
+    {
+        return $this->hasMany(OrderItem::class);
+    }
 }
