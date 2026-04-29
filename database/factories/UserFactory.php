bb3266de18239202681a46bfd0a42f25ff917a5f
puja-03 <pujakumari11th2003@gmail.com>
2026-04-29 12:54:34 +0530

    feat: add role assignment methods for admin and shopkeeper in UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,11 +29,26 @@ class UserFactory extends Factory
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
+            'role' => 'client',
             'remember_token' => Str::random(10),
             'two_factor_secret' => null,
             'two_factor_recovery_codes' => null,
             'two_factor_confirmed_at' => null,
         ];
+    }
+
+    public function admin(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'role' => 'admin',
+        ]);
+    }
+
+    public function shopkeeper(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'role' => 'shopkeeper',
+        ]);
     }
 
     /**
